2f585c7e4330e1bbebeb13d0375bad346e50d586
manuelillolg <manulopez18899@gmail.com>
2024-08-07 12:36:35 +0200

    bypassSecurityHtml added

--- src/app/iframe/iframe.component.ts
+++ src/app/iframe/iframe.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { DomSanitizer, SafeHtml, SafeResourceUrl } from '@angular/platform-browser';
 import { IframeService } from './iframe.service';
 import { CommonModule } from '@angular/common';
 
@@ -11,14 +11,18 @@ import { CommonModule } from '@angular/common';
   standalone : true
 })
 export class IframeComponent implements OnInit {
-  iframeSrc: string = '';
+  iframeSrc: SafeHtml = '';
 
-  constructor(private iframeService: IframeService) {}
+  constructor(
+    private iframeService: IframeService,
+    private sanitizer: DomSanitizer
+  ) {}
 
   ngOnInit() {
     this.iframeService.getIframeSrc().subscribe(
       (src) => {
-        this.iframeSrc = src;
+
+        this.iframeSrc = this.sanitizer.bypassSecurityTrustHtml(src)
       },
       (error) => {
         console.error('Error al obtener el src del iframe:', error);
